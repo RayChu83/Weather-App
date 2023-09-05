1c7fbefacca27eb7ad1e454443a2542e78d49cea
Ray <rayc12079@gmail.com>
2023-09-05 00:40:37 -0400

    Attempt 5 to fix CSS link to HTML

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", # Here
 
     'weather_app',
 ]
